8c98a7b8bf9b51d0d324298d3d914882ea613b14
Alan <alan_ps@hotmail.com>
2022-04-18 18:06:53 -0400

    iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require("../models/Drone.model")
 
-// require the Drone model here
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+router.get("/drones", (req, res, next) => {
+  Drone.find()
+    .then(function (allDrones) {
+      console.log("Found drones", allDrones);
+      res.render("drones/list", { allDrones: allDrones });
+    })
+    .catch(function (err) {
+      console.log("Something went wrong", err.message);
+    });
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form')
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  console.log("BODY", req.body);
+  Drone.create({ ...req.body })
+  .then(function(results) {
+    console.log("SUCCESS", results)
+    res.redirect("/drones");
+  })
+  .catch(function(error) {
+    console.log("FAILED", error.message);
+  })
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
